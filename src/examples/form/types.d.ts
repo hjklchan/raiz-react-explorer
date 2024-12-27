46a0dfdd437b078403ddc2a054b975748d2222ad
Lucas chen <lucas.chen@gbo-asia.com>
2024-12-27 16:35:41 +0800

    新增字段 (many2one, boolean, etc.) 的支持

--- src/examples/form/types.d.ts
+++ src/examples/form/types.d.ts
@@ -11,14 +11,63 @@ export interface RaizSelectElement {
   dependsOn?: string;
 }
 
+// VarcharField
+//
 // Inherit HTMLInputElement
 export interface VarcharField extends Field, RaizInputElement {
   type: "varchar";
 }
 
-export interface SelectionField extends Field, RaizSelectElement {
-    type: "selection";
-    options?: SelectProps["options"];
+// IntegerField
+//
+// Inherit HTMLInputElement
+export interface IntegerField extends Field, RaizInputElement {
+  type: "integer";
 }
 
-export type RaizField = VarcharField | SelectionField;
+// FloatField
+//
+// Inherit HTMLInputElement
+export interface FloatField extends Field, RaizInputElement {
+  type: "float";
+  precision?: number;
+}
+
+// EmailField
+// 
+// Inherit HTMLInputElement
+export interface EmailField extends Field, RaizInputElement {
+  type: "email";
+}
+
+// EmailField
+//
+// Inherit HTMLInputElement
+export interface BooleanField extends Field, RaizInputElement {
+  type: "boolean";
+}
+
+// SelectionField
+// 
+// Inherit HTMLSelectElement
+export interface SelectionField extends Field, RaizSelectElement {
+  type: "selection";
+  options?: SelectProps["options"];
+}
+
+// Many2oneField
+// 
+// Inherit HTMLSelectElement
+export interface Many2oneField extends Field, RaizSelectElement {
+  type: "many2one";
+  model: string;
+}
+
+export type RaizField =
+  | VarcharField
+  | FloatField
+  | IntegerField
+  | BooleanField
+  | EmailField
+  | Many2oneField
+  | SelectionField;

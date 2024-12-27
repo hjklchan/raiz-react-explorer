5273236b8a20d37952a9f45e2524f9e7b9464371
Lucas chen <lucas.chen@gbo-asia.com>
2024-12-27 16:35:27 +0800

    新增字段 (many2one, boolean, etc.) 的支持

--- src/examples/form/index.tsx
+++ src/examples/form/index.tsx
@@ -1,4 +1,12 @@
-import { Form } from "@arco-design/web-react";
+import {
+  Alert,
+  Button,
+  Form,
+  Input,
+  InputNumber,
+  Notification,
+  Switch,
+} from "@arco-design/web-react";
 import {
   createContext,
   useCallback,
@@ -10,92 +18,143 @@ import {
 import { Varchar } from "./components/Varchar";
 import { Selection } from "./components/Selection";
 import useForm from "@arco-design/web-react/es/Form/useForm";
-import { RaizField, RaizSelectElement, SelectionField } from "./types";
+import { RaizField } from "./types";
+import example_json from "./example.json";
+import Many2one from "./components/Many2one";
 
 export default function () {
   const [form] = useForm();
-  // const [values, setValues] = useState()
-  const [fields, _] = useState<RaizField[]>([
-    { type: "varchar", name: "username" },
-    {
-      type: "selection",
-      name: "Status",
-      options: [
-        {
-          label: "Open",
-          value: "open",
-        },
-        {
-          label: "Cancel",
-          value: "cancel",
-        },
-      ],
-    },
-    {
-      type: "selection",
-      name: "shipment_status",
-      label: "Shipment Status",
-      options: [
-        {
-          label: "Shipped",
-          value: "shipped",
-        },
-        {
-          label: "Waiting",
-          value: "waiting",
-        },
-      ],
-      dependsOn: "status",
-    },
-  ]);
+  const [fields, _] = useState<RaizField[]>(example_json as RaizField[]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    console.log("Index has been reloaded");
+  }, []);
 
-  const onChange = (field: string, value: any) => {
-    form.setFieldValue(field, value);
-    console.log(`Field ${field} has been changed to ${value}`);
+  const onChange = useCallback((f: string, v: any) => {
+    form.setFieldValue(f, v);
+    console.log(`Field ${f} has been changed to ${v}`);
     console.log(form.getFieldsValue());
-  };
+  }, []);
 
   const toDictValues = (
-    fields: Pick<RaizField, "name">[],
-    fn: (value: any) => any
+    fields: Pick<RaizField, "name" | "type">[],
+    defaultValueFrom: (type: RaizField["type"]) => any
   ) => {
     let dict: { [field: string]: any } = {};
 
     fields.forEach((field) => {
-      dict[field.name] = fn(undefined);
+      dict[field.name] = defaultValueFrom(field.type);
     });
 
     return dict;
   };
 
+  const formOnSubmit = (values: { [field: string]: any }) => {
+    console.log("Form submit", values);
+  };
+
   const formInitialValues = useMemo(() => {
-    return toDictValues(fields, (_) => "");
+    return toDictValues(fields, (t) => {
+      if (t === "boolean") return false;
+      return "";
+    });
   }, []);
 
   return (
-    <Form form={form} initialValues={formInitialValues}>
+    <Form form={form} initialValues={formInitialValues} onSubmit={formOnSubmit}>
       {fields.map((field) => {
         const label =
           field.label ??
           field.name.charAt(0).toUpperCase().concat(field.name.substring(1));
 
-        switch (field.type) {
+        const fieldType = field.type;
+
+        switch (fieldType) {
+          // Handle Varchar field
           case "varchar":
             return (
               <Form.Item key={field.name} label={label} field={field.name}>
                 <Varchar field={field} onChange={(f, v) => onChange(f, v)} />
               </Form.Item>
             );
+          // Handle Selection field
           case "selection":
             return (
-              <Form.Item key={field.name} label={label} field={field.name}>
+              <Form.Item
+                key={field.name}
+                label={label}
+                field={field.name}
+                // 如果存在依赖字段
+                // 则该字段会被重新渲染一次
+                shouldUpdate={field.dependsOn !== undefined}
+              >
                 <Selection field={field} onChange={(f, v) => onChange(f, v)} />
               </Form.Item>
             );
+    case "many2one":
+        return (
+              <Form.Item
+                key={field.name}
+                label={label}
+                field={field.name}
+                // 如果存在依赖字段
+                // 则该字段会被重新渲染一次
+                shouldUpdate={field.dependsOn !== undefined}
+              >
+                <Many2one field={field} />
+              </Form.Item>
+        )
+          // Handle Boolean field
+          case "boolean":
+            return (
+              <Form.Item key={field.name} field={field.name} label={label}>
+                <Switch />
+              </Form.Item>
+            );
+          // Handle Float field
+          case "float":
+            return (
+              <Form.Item key={field.name} field={field.name} label={label}>
+                <InputNumber precision={field.precision} />
+              </Form.Item>
+            );
+          // Handle Integer field
+          case "integer":
+            return (
+              <Form.Item key={field.name} field={field.name} label={label}>
+                <InputNumber precision={0} />
+              </Form.Item>
+            );
+          // Handle Email field
+          case "email":
+            return (
+              <Form.Item key={field.name} field={field.name} label={label}>
+                <Input placeholder="TODO" />
+              </Form.Item>
+            );
+          default:
+            return (
+              <Button
+                type="text"
+                status="danger"
+                size="mini"
+                onClick={() => {
+                  Notification.error({
+                    title: "Error",
+                    content: `Unknown field type '${fieldType}'`,
+                  });
+                }}
+              >
+                Error
+              </Button>
+            );
         }
       })}
+      <Form.Item>
+        <Button type="primary" onClick={form.submit}>
+          Save
+        </Button>
+      </Form.Item>
     </Form>
   );
 }

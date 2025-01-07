218044467d5a859ee0c977f529539afbe447e951
Lucas chen <lucas.chen@gbo-asia.com>
2025-01-07 17:42:38 +0800

    do some validations

--- src/examples/form/components/Picture.tsx
+++ src/examples/form/components/Picture.tsx
@@ -1,6 +1,6 @@
 import { Upload } from "@arco-design/web-react";
 import { PictureField } from "../types";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { UploadItem } from "@arco-design/web-react/es/Upload";
 
 export interface PictureProps {
@@ -11,14 +11,29 @@ export const Picture = (props: PictureProps) => {
   const [files, setFiles] = useState<UploadItem[]>();
 
   const {
-    field: { action },
+    field: { action, properties },
   } = props;
+
+  const validAndBuildAccept = useCallback((): string => {
+    const type = properties?.types ?? "";
+
+    const concatFn = (value: string) => "image/".concat(value);
+
+    if (typeof type === "string") {
+      return type.split(",").map(concatFn).join(", ");
+    } else if (type instanceof Array) {
+      return type.map(concatFn).join(", ");
+    } else {
+      return "image/*";
+    }
+  }, []);
 
   return (
     <Upload
       action={action}
       fileList={files ?? []}
       listType="picture-card"
+      accept={validAndBuildAccept()}
     />
   );
 };
